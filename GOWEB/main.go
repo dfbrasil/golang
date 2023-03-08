8d4d6310f7fe84ef0f53e61585a42c4f37ca570e
Daniel Brasil <danieldfb@gmail.com>
2023-03-08 09:09:27 -0300

    DElete

--- GOWEB/main.go
+++ GOWEB/main.go
@@ -10,6 +10,16 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+//ProductValidator scho validator for product
+type ProductValidator struct{
+	validator *validator.Validate
+}
+
+//Validate validates product request body
+func (p *ProductValidator) Validate(i interface{}) error { //assinatura desse método é igual a assinatura do validate ctrl + p
+	return p.validator.Struct(i)
+}
+
 func main() {
 
 	port := os.Getenv("MY_APP_PORT")
@@ -35,13 +45,13 @@ func main() {
 	e.GET("/products/:id", func(c echo.Context) error {
 
 		var product map[int]string
-
+		pID, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			return err
+		}		
 		for _, p := range products{
 			for k := range p{
-				pID, err := strconv.Atoi(c.Param("id"))
-				if err != nil {
-					return err
-				}
+				
 				if pID == k {
 					product = p
 				}
@@ -57,17 +67,15 @@ func main() {
 	e.POST("/products", func(c echo.Context) error {
 		type body struct{
 			Name string `json:"product_name" validate:"required,min=4"`
-			// Vendor string `json:"vendor" validate: "min=5,max=10"`
-			// Email string `json:"email" validate: "required_with=Vendor,email"`
-			// Website string `json:"website" validate:"url"`
-			// Country string `json:"country" validate: "len=2"`
-			// DefaultDeviceIp string `json:"default_device_ip" validate:"ip"`
 		}
 		var reqBody body
+
+		e.Validator = &ProductValidator{validator: v}
+
 		if err := c.Bind(&reqBody); err != nil {
 			return err
 		}
-		if err := v.Struct(reqBody); err != nil{
+		if err := c.Validate(reqBody); err != nil{
 			return err
 		}
 
@@ -77,6 +85,78 @@ func main() {
 		products = append(products, product)
 		return c.JSON(http.StatusOK, product)
 	})
+
+	e.PUT("/products/:id", func(c echo.Context) error {
+		var product map[int]string
+		pID, err := strconv.Atoi(c.Param("id"))
+		if err != nil{
+			return err
+		}
+
+		for _, p := range products{
+			for k := range p {
+				if pID == k {
+					product = p
+				}
+			}
+		}
+
+		if product == nil{
+			return c.JSON(http.StatusNotFound, "product not found")
+		}
+
+		type body struct{
+			Name string `json:"product_name" validate:"required,min=4"`
+		}
+		var reqBody body
+
+		e.Validator = &ProductValidator{validator: v}
+
+		if err := c.Bind(&reqBody); err != nil {
+			return err
+		}
+		if err := c.Validate(reqBody); err != nil{
+			return err
+		}
+
+		product[pID] = reqBody.Name
+		return c.JSON(http.StatusOK, product)
+	})
+
+	
+	e.DELETE("/products/:id", func(c echo.Context) error {
+
+		var product map[int]string
+		var index int
+		pID, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			return err
+		}		
+		for i, p := range products{
+			for k := range p{
+				if pID == k {
+					product = p
+					index = i
+				}
+			}
+		}
+
+		if product == nil {
+			return c.JSON(http.StatusNotFound, "Product not found")
+		}
+
+		splice := func (s []map[int]string, index int) []map[int]string {
+			return append(s[:index], s[index+1:]...)
+			//[1,2,3,4,5,6]
+			//[1,2]+[4,5,6] = [1,2,4,5,6] (idiomatic in go)
+		}
+
+		products = splice(products, index)
+
+		return c.JSON(http.StatusOK, product)
+	})
+
+
 
 
 	e.StdLogger.Printf(fmt.Sprintf("Rodando echo na porta %s", port))
