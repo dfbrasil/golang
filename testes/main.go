42e8c77e441046c5010e1a0cb3839a636805cc92
Daniel Brasil <danieldfb@gmail.com>
2023-03-09 15:00:04 -0300

    att

--- testes/main.go
+++ testes/main.go
@@ -4,6 +4,18 @@ import (
 	"fmt"
 )
 
+func Soma(valores ...int) (total int){
+	for _, valor := range valores{
+		total += valor
+	}
+	return
+}
+
+func JustPanic(b bool) {
+    if b {
+        panic("Fire!!! Fire!!! Fire!!!")
+    }
+}
 
 func main()  {
 	fmt.Println("Pasta de testes")
--- testes/main_test.go
+++ testes/main_test.go
@@ -7,5 +7,28 @@ import (
 )
 
 func TestSomething(t *testing.T)  {
-	assert.False(t, true, "true is true")
+	assert.True(t, true, "true is true")
+}
+
+func TestSoma(t *testing.T) {
+	
+	testes := []struct {
+		Valores []int
+		Resultado int
+	}{
+		{Valores: []int{1, 2, 3}, Resultado: 6},
+        {Valores: []int{1, 2, 3, 4}, Resultado: 10},
+	}
+
+	for _, teste := range testes {
+        total := Soma(teste.Valores...)
+
+		assert.Equal(t, total, teste.Resultado, "Valor esperado: %d - Valor retornado: %d", teste.Resultado, total)
+    }
+}
+
+func TestJustPanic(t *testing.T) {
+    assert := assert.New(t)
+    assert.Panics(func() { JustPanic(true) })
+    assert.NotPanics(func() { JustPanic(false) })
 }
