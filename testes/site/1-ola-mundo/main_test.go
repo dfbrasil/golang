f1a57c2add8dc81c433166c0ce525947e4ede6d1
Daniel Brasil <danieldfb@gmail.com>
2023-03-09 23:55:57 -0300

    adição de helper

--- testes/site/1-ola-mundo/main_test.go
+++ testes/site/1-ola-mundo/main_test.go
@@ -3,22 +3,23 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
+
+	verificaMensagemCorreta := func (t testing.T, resultado, esperado string)  {
+		t.Helper()
+		if resultado != esperado{
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
+	}
 	
 	t.Run("diz olá para as pessoas", func(t *testing.T) {
 		resultado := Ola("Daniel")
 		esperado := "Olá Daniel"
-
-		if resultado != esperado{
-			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-		}
+		verificaMensagemCorreta(*t, resultado, esperado)
 	})
 
 	t.Run("Diz Olá Mundo quando uma string vazia for passada", func(t *testing.T) {
 		resultado := Ola("")
 		esperado := "Olá mundo"
-
-		if resultado != esperado {
-			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-		}
+		verificaMensagemCorreta(*t, resultado, esperado)
 	})
 }
