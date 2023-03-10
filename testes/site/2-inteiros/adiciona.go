71526c4d8f4b16d9ff80b3a02351dd564eabb55c
Daniel Brasil <danieldfb@gmail.com>
2023-03-10 17:24:14 -0300

    inteiros

---
+++ testes/site/2-inteiros/adiciona.go
@@ -0,0 +1,8 @@
+package inteiros
+
+
+// Adiciona recebe dois inteiros como argumento e retorna a soma deles
+func Adiciona(a, b int) int {
+	soma := a + b
+	return soma
+}
---
+++ testes/site/2-inteiros/go.mod
@@ -0,0 +1,3 @@
+module inteiros
+
+go 1.18
