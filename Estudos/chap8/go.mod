d2041f8233871d09d2c435573695a50b23f6a5b7
Daniel Brasil <danieldfb@gmail.com>
2023-03-27 07:18:31 -0300

    n

--- Estudos/chap8/go.mod
+++ Estudos/chap8/go.mod
@@ -1,3 +1,3 @@
-module chap5
+module chap8
 
 go 1.18
--- Estudos/chap9/main9.go
+++ Estudos/chap9/main9.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// var hotelName string = "Hotel California"
+// var roomsAvailable, rooms, roomsOccupied uint8
+
+
+func idade() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	var ageJonh, agePaul uint8 =  uint8(rand.Intn(100)), uint8(rand.Intn(100))
+	fmt.Printf("Jonh tem %v anos.\n", ageJonh)
+	fmt.Printf("Paul tem %v anos.\n", agePaul)
+	if ageJonh > agePaul{
+		fmt.Println("É verdade que João é mais velho que Paul")
+	} else if ageJonh < agePaul{
+		fmt.Println("É verdade que Paulo seja mais velho que Jonh.")
+	} else{
+		fmt.Println("Ambos tem a mesma idade.")
+	}
+}
 
 func main()  {
-	fmt.Println("tal")
+	// fmt.Printf("O nome do hotel é: %v\n", hotelName)
+	// fmt.Println("Insira o número de quartos:")
+	// fmt.Scanf("%v\n", &rooms)
+	// fmt.Println("Insira o número de quartos ocupados:")
+	// fmt.Scanf("%v\n", &roomsOccupied)
+	// if roomsOccupied > rooms{
+	// 	fmt.Println("Número de quartos ocupados maior que o disponível")
+	// }
+	// roomsAvailable = rooms - roomsOccupied
+	// if roomsAvailable == 0{
+	// 	fmt.Println("Não há leitos disponíveis")
+	// } else{
+	// 	fmt.Printf("Existem %v disponíveis \n", roomsAvailable )
+	// }
+	idade()
 }
